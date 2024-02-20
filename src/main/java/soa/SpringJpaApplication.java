944bc7315883558205777dc3eb0385c11519782d
Moetaz0 <metzab15@gmail.com>
2024-02-21 00:53:47 +0100

    init

--- src/main/java/soa/SpringJpaApplication.java
+++ src/main/java/soa/SpringJpaApplication.java
@@ -6,21 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-
 @SpringBootApplication
-public class SpringJpaApplication
-{
-
+public class SpringJpaApplication {
 
     public static void main(String[] args) throws ParseException {
+        ApplicationContext context = SpringApplication.run(SpringJpaApplication.class, args);
 
-        ApplicationContext contexte = SpringApplication.run(SpringJpaApplication.class, args);
-
-
-
-
-
-
+        // Display a message upon application startup
+        System.out.println("Spring Boot application started successfully!");
     }
-
 }
